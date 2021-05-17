3ba990ead01b2c2702754cd7969074fc861dbc2e
Danny Calise <dannyjcalise@gmail.com>
2021-05-17 10:59:34 -0400

    DSN data manually inputted onto chart

--- gantt-vue/src/components/Gantt.vue
+++ gantt-vue/src/components/Gantt.vue
@@ -3,52 +3,107 @@
 </template>
 
 <script>
-import {gantt} from 'dhtmlx-gantt';
+import { gantt } from "dhtmlx-gantt";
 // import moment from 'moment';
+
 export default {
-  name: 'gantt',
+  name: "gantt",
   props: {
     tasks: {
       type: Object,
-      default () {
-        return {data: [], links: []}
-      }
-    }
+      default() {
+        return { data: [], links: [] };
+      },
+    },
   },
-  mounted: function () {
+  mounted: function() {
     gantt.config.xml_date = "%Y-%m-%d";
-    var contactEventLabel = [
-    "<div class='gantt-text-label'>"+
-    "<span>Contact Event</span>" +
-    "</div>"
-].join("");
-    var text1Label = [
-    "<div class='gantt-text-label'>"+
-    "<span>Epoch Entry</span>" +
-    "</div>"
-].join("");
-var text2Label = [
-    "<div class='gantt-text-label'>"+
-    "<span>Exit Entry</span>" +
-    "</div>"
-].join("");
+    var nameLabel = [
+      "<div class='gantt-text-label'>" +
+        "<span>Contact Event</span>" +
+        "</div>",
+    ].join("");
 
+    var entryLabel = [
+      "<div class='gantt-text-label'>" + "<span>Epoch Entry</span>" + "</div>",
+    ].join("");
+    var exitLabel = [
+      "<div class='gantt-text-label'>" + "<span>Exit Entry</span>" + "</div>",
+    ].join("");
+     var durationLabel = [
+      "<div class='gantt-text-label'>" + "<span>Duration</span>" + "</div>",
+    ].join("");
 
-gantt.config.columns = [
-    {name: "text", label:contactEventLabel, align: "center"},
-    {name: "text1", label:text1Label, align: "center"},
-    {name: "text2", label:text2Label, align: "center"},
-    // {name: "start_time", align: "center"},
-    {name: "duration", align: "center"},
-    {name: "add", width: 44}
-],
-    gantt.init(this.$refs.gantt);
+    var firstMonth = 1,
+      firstDay = 1;
+
+    gantt.date.fiscal_year_start = function(date) {
+      var next = new Date(date);
+      if (
+        next.getMonth() < firstMonth ||
+        (next.getMonth() === firstMonth && next.getDate() < firstDay)
+      ) {
+        next = gantt.date.add(next, -1, "year");
+      }
+
+      next = gantt.date.year_start(next);
+      next.setMonth(firstMonth);
+      next.setDate(firstDay);
+
+      return next;
+    };
+
+    gantt.date.add_fiscal_year = function(date, inc) {
+      return gantt.date.add(date, inc, "year");
+    };
+
+    var dateToStr = gantt.date.date_to_str("%Y");
+    function fiscalYearLabel(date) {
+      return dateToStr(gantt.date.fiscal_year_start(date));
+    }
+
+    gantt.config.scales = [
+      { unit: "year", step: 1, format: "Calendar year %Y" },
+      { unit: "fiscal_year", step: 1, format: fiscalYearLabel },
+      { unit: "month", step: 1, format: "%M %Y" },
+      { unit: "day", step: 1, format: "%d %M" },
+    ];
+
+    (gantt.config.columns = [
+      { name: "text", label: nameLabel, align: "center" },
+      { name: "entry_epoch", label: entryLabel, align: "center" },
+      { name: "exit_epoch", label: exitLabel, align: "center" },
+      { name: "duration", label: durationLabel, align: "center" },
+      { name: "add", width: 44 },
+    ]),
+      // (gantt.config.scales = [
+      //   {
+      //     unit: "week",
+      //     step: 1,
+      //     format: function(date) {
+      //       return "Week #" + gantt.date.getWeek(date);
+      //     },
+      //   },
+      //   {
+      //     unit: "day",
+      //     step: 1,
+      //     format: "%D",
+      //     css: function(date) {
+      //       if (!gantt.isWorkTime({ date: date, unit: "day" })) {
+      //         return "weekend";
+      //       }
+      //     },
+      //   },
+      //   { unit: "hour", step: 1 },
+      //   { unit: "minute", step: 1 },
+      // ]);
+      gantt.init(this.$refs.gantt);
     gantt.parse(this.$store.getters.timeline);
 
     // console.log(moment('24/12/2019 09:15:00', "DD MM YYYY hh:mm:ss"));
     // console.log(moment("12-25-1995", "MM-DD-YYYY"));
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
--- gantt-vue/src/main.js
+++ gantt-vue/src/main.js
@@ -9,20 +9,22 @@ const store = new Vuex.Store({
   state: {
     /** THIS NEEDS TO BE DSN ENTRIES (use 5) */
     activities: [
-      { id: 1, text1: "2020-01-17", duration: 1, text: "Contact Event #1", progress: 0.5 },
+      { id: 1, start_date: "2021-02-20", duration: 1.35000, text: "Event #1", progress: 0.5, entry_epoch: "2021-051/03:45:00", exit_epoch: "2021-051/03:46:21" },
       //
-      { id: 2, start_date: "2020-01-20", duration: 1, text: "Contact Event #2", progress: 0.5 },
+      { id: 2, start_date: "2021-02-21", duration: 524.21667, text: "Event #2", progress: 0.5, entry_epoch: "2021-052/06:50:39", exit_epoch: "2021-052/15:34:52" },
       //
-      { id: 3, start_date: "2020-01-23", duration: 1, text: "Contact Event #3", progress: 0.5 },
+      { id: 3, start_date: "2021-02-22", duration: 331.46667, text: "Event #3", progress: 0.5, entry_epoch: "2021-053/02:14:25", exit_epoch: "2021-053/07:45:53" },
       //
+      { id: 4, start_date: "2021-02-23", duration: 453.81667, text: "Event #4", progress: 0.5, entry_epoch: "2021-054/03:20:59", exit_epoch: "2021-054/10:54:48" },
+
+      { id: 5, start_date: "2021-02-24", duration: 582.41667, text: "Event #5", progress: 0.5, entry_epoch: "2021-055/05:27:25", exit_epoch: "2021-055/15:09:50" },
     ]
   },
   mutations: {
   },
   getters: {
     timeline(state) {
-      // { id: 1, start_date: "2020-01-17", duration: 1, text: "Task #1", progress: 0.5 },
-      const data = state.activities.map(toGanttData);
+     const data = state.activities.map(toGanttData);
       console.log(data);
       return {
         data: data,
@@ -33,7 +35,7 @@ const store = new Vuex.Store({
 });
 
 function toGanttData(activity) {
-  const gantt = {}
+  // const gantt = {}
   return activity;
 }
 
